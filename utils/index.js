479b490341fc6273ff9c76ba78e71f31a0082a17
projetoSylog <fabio@sylog.com.br>
2025-07-14 15:58:06 -0300

    pcommit

---
+++ utils/index.js
@@ -0,0 +1,42 @@
+const meli = require('mercadolibre');
+require('dotenv').config();
+
+const { CLIENT_ID, CLIENT_SECRET } = process.env;
+
+const MeliObject = function meliObject(access_token) {
+  this.meli = new meli.Meli(CLIENT_ID, CLIENT_SECRET, access_token);
+};
+
+MeliObject.prototype.get = function meli_get(url, params = null) {
+  return new Promise((resolve, reject) => {
+    this.meli.get(url, params, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
+
+MeliObject.prototype.post = function meli_post(url, body, params = null) {
+  return new Promise((resolve, reject) => {
+    this.meli.post(url, body, params, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
+
+// Função que usa o objeto MeliObject para fazer a requisição GET
+async function meli_get(meliObject, url, params = null) {
+  return await meliObject.get(url, params);
+}
+
+module.exports = {
+  MeliObject,
+  meli_get // agora está exportando a função esperada
+};
